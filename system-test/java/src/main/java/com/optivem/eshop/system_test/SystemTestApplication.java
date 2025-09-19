82a96d708217ebb7b5d8487fed0a8feb7457002f
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-09-19 15:07:28 +0200

    feat: Initialize system-test project with Gradle wrapper and Spring Boot
    - Added Gradle wrapper properties and scripts for project setup.
    - Created initial Spring Boot application class.
    - Configured application properties for the Spring Boot application.
    - Added a basic test class to verify application context loading.
    - Set up project structure with necessary directories and files.

---
+++ system-test/java/src/main/java/com/optivem/eshop/system_test/SystemTestApplication.java
@@ -0,0 +1,13 @@
+package com.optivem.eshop.system_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemTestApplication.class, args);
+	}
+
+}
---
+++ system-test/java/src/test/java/com/optivem/eshop/system_test/SystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optivem.eshop.system_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
