4f53b7fa20d5d8f96fc70757d1343257fe9899d5
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-09-19 16:11:53 +0200

    fix: add --info flag to smoke test command and remove outdated Google test

--- system-test/java/src/test/java/com/optivem/eshop/smoketest/SmokeTest.java
+++ system-test/java/src/test/java/com/optivem/eshop/smoketest/SmokeTest.java
@@ -8,17 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SmokeTest {
     @Test
-    void shouldOpenGoogleAndCheckTitle() {
-        try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch();
-            Page page = browser.newPage();
-            Response response = page.navigate("https://www.google.com");
-            assertEquals(200, response.status());
-            browser.close();
-        }
-    }
-
-    @Test
     void shouldOpenLocalhostAndCheckStatus200() {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch();
